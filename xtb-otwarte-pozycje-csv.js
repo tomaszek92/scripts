d8f4e6871006b18d82de6f86fcd38b28245f0cf1
tomaszek92 <tomaszek92@users.noreply.github.com>
2023-11-28 15:23:42 +0100

    Update xtb-otwarte-pozycje-csv.js

--- xtb-otwarte-pozycje-csv.js
+++ xtb-otwarte-pozycje-csv.js
@@ -1,17 +1,17 @@
 (function () {
-    const rows = document.querySelectorAll('.grid-canvas > .ui-widget-content.slick-group');
-    
-    const array = Array.from(rows).map(row => {
-        const name =  row.children[0].children[1].children[0].childNodes[0].textContent;
-        
-        const valueText = row.children[2].innerText.replaceAll(' ', '');
-        const value = parseFloat(valueText);
+  const rows = document.querySelectorAll(".grid-canvas > .ui-widget-content.slick-group");
 
-        return [name, value];
+  const array = Array.from(rows)
+    .filter((row) => row.children[2])
+    .map((row) => {
+      const name = row.children[0].children[1].children[0].childNodes[0].textContent;
+      const valueText = row.children[2].innerText.replaceAll(" ", "");
+      const value = parseFloat(valueText);
+
+      return [name, value];
     });
 
-    const csvContent = 'data:text/csv;charset=utf-8,' + array.map(row => row.join(',')).join('\n');
-    const encodedUri = encodeURI(csvContent);
-    window.open(encodedUri);
-  })()
-  
+  const csvContent = "data:text/csv;charset=utf-8," + array.map((row) => row.join(",")).join("\n");
+  const encodedUri = encodeURI(csvContent);
+  window.open(encodedUri);
+})();
